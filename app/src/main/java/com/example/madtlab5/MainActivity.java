55b54f5fbb945063b01b6a8bad3aa748219ad59a
Serhii <71066322+bonfletcher15@users.noreply.github.com>
2024-01-01 13:20:01 +0200

    Update MainActivity.java

--- app/src/main/java/com/example/madtlab5/MainActivity.java
+++ app/src/main/java/com/example/madtlab5/MainActivity.java
@@ -15,33 +15,35 @@ public class MainActivity extends AppCompatActivity
     private ArrayAdapter<String> adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState)
-    {
+{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         listView = findViewById(R.id.listView);
         searchEditText = findViewById(R.id.searchEditText);
         DataInput dataInput = new DataInput(this, listView);
-        dataInput.execute("https://www.floatrates.com/daily/usd.xml");
+        dataInput.execute("https://www.gov.uk/bank-holidays.json");
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener()
-        {
+{
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id)
-            {
+{
                 String selectedItem = (String) parent.getItemAtPosition(position);
                 Toast.makeText(MainActivity.this, "Selected: " + selectedItem, Toast.LENGTH_SHORT).show();
             }
         });
     }
+
     public void filterList(View view)
-    {
+{
         String searchText = searchEditText.getText().toString().toUpperCase();
         if (adapter != null)
-        {
+{
             adapter.getFilter().filter(searchText);
         }
     }
+
     public void setAdapter(ArrayList<String> data)
-    {
+{
         adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, data);
         listView.setAdapter(adapter);
     }
